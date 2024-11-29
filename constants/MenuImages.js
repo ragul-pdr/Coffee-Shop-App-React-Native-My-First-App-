1b26541aeaaa2430eb2e1f6aa4bd8f8fe66c3ed1
FIRST_NAME LAST_NAME <ragulmaha81@gmail.com>
2024-11-29 14:41:48 +0530

    Images Updated

--- constants/MenuImages.js
+++ constants/MenuImages.js
@@ -2,7 +2,17 @@ import c1 from '@/assets/images/menu/coffee1.jpeg';
 import c2 from '@/assets/images/menu/coffee2.jpeg';
 import c3 from '@/assets/images/menu/coffee3.jpeg';
 import c4 from '@/assets/images/menu/coffee4.jpg';
+import c5 from '@/assets/images/menu/c5.jpg'
+import c6 from '@/assets/images/menu/c6.jpg'
+import c7 from '@/assets/images/menu/c7.jpg'
+import c8 from '@/assets/images/menu/c8.jpg'
+import c9 from '@/assets/images/menu/c9.jpg'
+import c10 from '@/assets/images/menu/c10.jpg'
+import c11 from '@/assets/images/menu/c11.jpg'
+import c12 from '@/assets/images/menu/c12.jpg'
+import c13 from '@/assets/images/menu/c13.jpg'
+import c14 from '@/assets/images/menu/c14.jpg'
 
 export default [
-    c1,c2,c3,c4
+    c1,c2,c3,c4,c5,c6,c7,c8,c9,c10,c11,c12,c13,c14,c1,c2,c3,c4,c5,c6
 ]
--- constants/MenuItems.js
+++ constants/MenuItems.js
@@ -1,55 +1,102 @@
-export const MENU_ITEMS=[
+export const MENU_ITEMS = [
     {
-        "id":1,
-        "title":"Starbuks",
-        "description":"Costly and high Quality"
+        "id": 1,
+        "title": "Espresso",
+        "description": "Strong, concentrated coffee."
     },
     {
-        "id":1,
-        "title":"Starbuks",
-        "description":"Costly and high Quality"
+        "id": 2,
+        "title": "Americano",
+        "description": "Espresso diluted with hot water."
     },
     {
-        "id":1,
-        "title":"Starbuks",
-        "description":"Costly and high Quality"
+        "id": 3,
+        "title": "Latte",
+        "description": "Espresso with steamed milk and a light layer of foam."
     },
     {
-        "id":1,
-        "title":"Starbuks",
-        "description":"Costly and high Quality"
+        "id": 4,
+        "title": "Cappuccino",
+        "description": "Espresso with equal parts steamed milk and foam."
     },
     {
-        "id":1,
-        "title":"Starbuks",
-        "description":"Costly and high Quality"
+        "id": 5,
+        "title": "Macchiato",
+        "description": "Espresso topped with a dollop of foam."
     },
     {
-        "id":1,
-        "title":"Starbuks",
-        "description":"Costly and high Quality"
+        "id": 6,
+        "title": "Mocha",
+        "description": "Espresso with chocolate syrup and steamed milk."
     },
     {
-        "id":1,
-        "title":"Starbuks",
-        "description":"Costly and high Quality"
+        "id": 7,
+        "title": "Flat White",
+        "description": "Espresso with micro-foamed milk."
     },
     {
-        "id":1,
-        "title":"Starbuks",
-        "description":"Costly and high Quality"
+        "id": 8,
+        "title": "Ristretto",
+        "description": "A shorter, more concentrated shot of espresso."
     },
     {
-        "id":1,
-        "title":"Starbuks",
-        "description":"Costly and high Quality"
+        "id": 9,
+        "title": "Affogato",
+        "description": "Espresso poured over a scoop of vanilla ice cream."
     },
     {
-        "id":1,
-        "title":"Starbuks",
-        "description":"Costly and high Quality"
+        "id": 10,
+        "title": "Irish Coffee",
+        "description": "Coffee mixed with Irish whiskey, sugar, and cream."
     },
-]
-
-
-//React native List Views ===21:44
+    {
+        "id": 11,
+        "title": "Cold Brew",
+        "description": "Coffee brewed slowly with cold water."
+    },
+    {
+        "id": 12,
+        "title": "Nitro Coffee",
+        "description": "Cold brew infused with nitrogen gas."
+    },
+    {
+        "id": 13,
+        "title": "Iced Coffee",
+        "description": "Coffee served over ice."
+    },
+    {
+        "id": 14,
+        "title": "Frappe",
+        "description": "Blended coffee with milk and ice, often topped with whipped cream."
+    },
+    {
+        "id": 15,
+        "title": "Doppio",
+        "description": "A double shot of espresso."
+    },
+    {
+        "id": 16,
+        "title": "Cortado",
+        "description": "Espresso cut with a small amount of warm milk."
+    },
+    {
+        "id": 17,
+        "title": "Turkish Coffee",
+        "description": "Unfiltered coffee brewed in a special pot called a cezve."
+    },
+    {
+        "id": 18,
+        "title": "Viennese Coffee",
+        "description": "Coffee topped with whipped cream."
+    },
+    {
+        "id": 19,
+        "title": "Espresso Con Panna",
+        "description": "Espresso topped with whipped cream."
+    },
+    {
+        "id": 20,
+        "title": "Caramel Macchiato",
+        "description": "Espresso with steamed milk, caramel, and vanilla syrup."
+    }
+];
